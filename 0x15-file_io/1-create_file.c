9a9e7362fac447a3a1ff081cdd534a3af13c538e
gnxrlyqf <gnarlydestructor@gmail.com>
2023-07-11 01:54:34 +0100

    12

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,8 @@ int create_file(const char *filename, char *text_content)
 	ssize_t w;
 	int size = 0;
 
+	if (filename == NULL)
+		return (-1);
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content != NULL)
 	{
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 
 /**
- * create_file - crates a file and appends text to it
+ * append_text_to_file - crates a file and appends text to it
  * @filename: name of the file to create
  * @text_content: content to append to the file
  *
  * Return: 1 (Success) 0 (fail)
 */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int o;
 	int w;
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	o = open(filename, O_WRONLY | O_APPEND);
 	if (text_content != NULL)
 	{
 		for (size = 0; text_content[size];)
 			size++;
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, size);
 	if (w == -1 || o == -1)
 		return (-1);
