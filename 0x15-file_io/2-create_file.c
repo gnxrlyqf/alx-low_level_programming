52df791ca4c66e5c8228da7d3a8deaee8afb564c
gnxrlyqf <gnarlydestructor@gmail.com>
2023-07-11 01:30:59 +0100

    1

--- 0x15-file_io/2-create_file.c
+++ 0x15-file_io/2-create_file.c
@@ -12,20 +12,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t o;
-	ssize_t w;
+	int o;
+	int w;
 	int size = 0;
 
 	if (filename == NULL)
 		return (-1);
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content != NULL)
 	{
 		for (size = 0; text_content[size];)
 			size++;
 	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, size);
-	if (o == -1 || w == -1)
+	if (w == -1 || o == -1)
 		return (-1);
 	close(o);
 	return (1);
