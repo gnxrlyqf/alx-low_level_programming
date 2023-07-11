8d127ed8ff2aafa5d878af2a5984b544a2a80b8c
gnxrlyqf <gnarlydestructor@gmail.com>
2023-07-11 03:14:33 +0100

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,6 +43,11 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
 				exit(99);
 		r = read(from, &buffer, 1024);
+		if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	}
 	if (w == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
