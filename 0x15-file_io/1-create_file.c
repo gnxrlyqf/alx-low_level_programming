bcc06e789faa2bc03395c19d15fc61b195aa9553
gnxrlyqf <gnarlydestructor@gmail.com>
2023-07-11 01:56:51 +0100

    12

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	w = write(o, text_content, size);
 	if (o == -1 || w == -1)
-		return (0);
+		return (-1);
 	close(o);
 	return (1);
 }
