01faeecc89d162578a08e3b49aace88478e563d2
gnxrlyqf <gnarlydestructor@outlook.com>
2023-09-08 22:11:36 +0100

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -28,8 +28,6 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				printf("'%s': '%s'", current->key, current->value);
 				current = current->next;
-				if (!current)
-					printf(", ");
 			}
 			comma = 1;
 		}
