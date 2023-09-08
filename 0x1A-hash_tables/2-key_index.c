524f69fec11d6e54f957c4439433fde9eb918969
gnxrlyqf <gnarlydestructor@outlook.com>
2023-09-08 17:15:51 +0100

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - calculates the index in which the key will be placed
+ * @key: string to process
+ * @size: size of the array
+ *
+ * Return: array index
+*/
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
