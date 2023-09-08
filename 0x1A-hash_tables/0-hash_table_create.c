52ffec840a698ff5ec2d75d8e88281110c0534ce
gnxrlyqf <gnarlydestructor@outlook.com>
2023-09-08 16:24:10 +0100

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include "stdlib.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	long unsigned int i;
+	hash_table_t *new = malloc(sizeof(hash_table_t));
+
+	if (!new)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (!(new->array))
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
+	return (new);
+}
